c603529811974f37eb8a8ead53d399105b6909fc
Roman Pushkin <roman.pushkin@gmail.com>
2014-09-23 23:23:16 +0400

    Add index view with blue button

--- app.rb
+++ app.rb
@@ -3,5 +3,5 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb :index
 end
