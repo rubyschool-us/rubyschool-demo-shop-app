b84681b5aa5bc3b135042595130d2a9d3cbc5151
Roman Pushkin <roman.pushkin@gmail.com>
2014-09-25 22:58:10 +0400

    Keep x variable in localStorage

--- public/app.js
+++ public/app.js
@@ -1,8 +1,11 @@
 function something()
 {
-	var x = window.localStorage.getItem('aaa');
 
-	window.localStorage.setItem('aaa', 555);
+	var x = window.localStorage.getItem('bbb');
+
+	x = x * 1 + 1;
+
+	window.localStorage.setItem('bbb', x);
 
 	alert(x);
 }
