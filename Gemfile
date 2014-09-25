bb152f63bb7b08f31f0e72fe0f50b18474c417ac
Roman Pushkin <roman.pushkin@gmail.com>
2014-09-25 23:09:46 +0400

    Add Rakefile, update Gemfile, add Product model

--- Gemfile
+++ Gemfile
@@ -1,3 +1,11 @@
 source "https://rubygems.org"
 
 gem "sinatra"
+gem "sqlite3"
+gem "activerecord"
+gem "sinatra-activerecord"
+gem "sinatra-contrib"
+
+group :development do
+	gem "tux"
+end
--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:pizzashop.db"
+
+class Product < ActiveRecord::Base
+end
 
 get '/' do
 	erb :index
