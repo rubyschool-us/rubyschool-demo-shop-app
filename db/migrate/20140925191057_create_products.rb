23fc579668185f6b8c320c5595e91ca4630e2580
Roman Pushkin <roman.pushkin@gmail.com>
2014-09-25 23:23:47 +0400

    Create product migration

---
+++ db/migrate/20140925191057_create_products.rb
@@ -0,0 +1,18 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+
+  	create_table :products do |t|
+		t.string :title
+		t.text :description
+		t.decimal :price
+		t.decimal :size
+		t.boolean :is_spicy
+		t.boolean :is_veg
+		t.boolean :is_best_offer
+		t.string :path_to_image
+
+  		t.timestamps
+  	end
+
+  end
+end
