dd4710e3b41ca85d612413d08f9f96eb3a1b5fbf
Roman Pushkin <roman.pushkin@gmail.com>
2014-09-30 23:40:59 +0400

    Update add_to_cart function

--- public/app.js
+++ public/app.js
@@ -12,5 +12,9 @@ function something()
 
 function add_to_cart(id)
 {
-	alert('You added pizza with id: ' + id);
+	var key = 'product_' + id;
+
+	var x = window.localStorage.getItem(key);
+	x = x * 1 + 1;
+	window.localStorage.setItem(key, x);
 }
