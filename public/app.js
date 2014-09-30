fdcab586c20e445205be9b761ea9dfabe9d20f21
Roman Pushkin <roman.pushkin@gmail.com>
2014-09-30 22:43:55 +0400

    Display products, step 3, add image

--- public/app.js
+++ public/app.js
@@ -1,11 +1,11 @@
 function something()
 {
 
-	var x = window.localStorage.getItem('bbb');
+	var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
 
-	x = x * 1 + 1;
+	x = x * 1 + 1; // x = x + 1
 
-	window.localStorage.setItem('bbb', x);
+	window.localStorage.setItem('bbb', x); // hh['bbb'] = x
 
 	alert(x);
 }
