aee14254f7af19d3b5809209c0b18f29acee8f7f
Roman Pushkin <roman.pushkin@gmail.com>
2014-10-02 23:24:59 +0400

    Update orders button

--- public/app.js
+++ public/app.js
@@ -19,12 +19,19 @@ function add_to_cart(id)
 	window.localStorage.setItem(key, x);
 
 	update_orders_input();
+	update_orders_button();
 }
 
 function update_orders_input()
 {
 	var orders = cart_get_orders();
 	$('#orders_input').val(orders);
+}
+
+function update_orders_button()
+{
+	var text = 'Cart (' + cart_get_number_of_items() + ')';
+	$('#orders_button').val(text);
 }
 
 function cart_get_number_of_items()
