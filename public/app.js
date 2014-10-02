16a18ec6c076ae5bf2c51da0e02ffacd97dcf663
Roman Pushkin <roman.pushkin@gmail.com>
2014-10-02 23:08:22 +0400

    Add cart_get_orders

--- public/app.js
+++ public/app.js
@@ -39,3 +39,23 @@ function cart_get_number_of_items()
 
 	return cnt;
 }
+
+function cart_get_orders()
+{
+	var orders = '';
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); // получаем ключ
+		var value = window.localStorage.getItem(key); // получаем значение, аналог в ruby: hh[key] = x
+
+		if(key.indexOf('product_') == 0)
+		{
+			orders = orders + key + '=' + value + ',';
+		}
+	}
+
+	return orders;
+}
+
+
