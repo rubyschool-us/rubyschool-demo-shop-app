5862b5b16a8ed9e4298255efc14e699891818a3c
Roman Pushkin <roman.pushkin@gmail.com>
2014-10-09 23:02:58 +0400

    Refactoring: rename control

--- app.rb
+++ app.rb
@@ -26,7 +26,7 @@ post '/place_order' do
 end
 
 post '/cart' do
-  @orders_input = params[:orders]
+  @orders_input = params[:orders_input]
   @items = parse_orders_input @orders_input
 
   @items.each do |item|
