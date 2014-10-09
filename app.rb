371b06e5a8292bd493660362dd3c2a252630c053
Roman Pushkin <roman.pushkin@gmail.com>
2014-10-09 23:01:51 +0400

    Save order to database, display "thank you", clear localStorage

--- app.rb
+++ app.rb
@@ -20,6 +20,11 @@ get '/about' do
 	erb :about
 end
 
+post '/place_order' do
+  @order = Order.create params[:order]
+  erb :order_placed
+end
+
 post '/cart' do
   @orders_input = params[:orders]
   @items = parse_orders_input @orders_input
