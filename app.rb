59036c77ba88e37d0bae378071a781bd7c2f45fd
Roman Pushkin <roman.pushkin@gmail.com>
2014-10-09 23:32:41 +0400

    Display message if cart is empty

--- app.rb
+++ app.rb
@@ -26,13 +26,26 @@ post '/place_order' do
 end
 
 post '/cart' do
+
+  # получаем список параметров и разбираем (parse) их
+
   @orders_input = params[:orders_input]
   @items = parse_orders_input @orders_input
+
+  # выводим сообщение о том, что корзина пуста
+
+  if @items.length == 0
+    return erb :cart_is_empty
+  end
+
+  # выводим список продуктов в корзине
 
   @items.each do |item|
     # id, cnt
     item[0] = Product.find(item[0])
   end
+
+  # возвращаем представление по-умолчанию
 
 	erb :cart
 end
